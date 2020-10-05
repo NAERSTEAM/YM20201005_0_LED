1967ab8b9c4de4b9f0e817202121c037af9131b6
TIK <firmament_solo@hotmail.com>
2020-10-05 11:56:53 +0800

    created this file based on LAB_20200226_0

---
+++ YM20201005_0_LED.ino
@@ -0,0 +1,14 @@
+//OCT 5 2020 T.I. created this file based on LAB_20200226_0
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ digitalWrite(2,HIGH);
+ delay(1000);
+ digitalWrite(2,LOW);
+ delay(1000);
+}
